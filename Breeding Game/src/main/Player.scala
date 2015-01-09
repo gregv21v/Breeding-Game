0fdc08e8b7ff3a185095531c246965dee54994de
gregv21v <gregv21v@gmail.com>
2015-01-08 16:29:53 -0800

    .

---
+++ Breeding Game/src/main/Player.scala
@@ -0,0 +1,27 @@
+package main
+
+class Player {
+  var position = new PVector(0, 0, 0)
+  val speed = 10
+  
+  
+  
+  
+  
+  def moveLeft {
+    
+  }
+  
+  def moveRight {
+    
+  }
+  
+  
+  def moveUp {
+    
+  }
+  
+  def moveDown {
+    
+  }
+}
